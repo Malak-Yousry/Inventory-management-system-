64837f1298ae0f4a1a87ace21b88ea5a19c1eb9c
HannahEmad <hannahemadeldin@gmail.com>
2025-10-19 23:58:18 +0300

    Add files via upload

--- AdminRole.java
+++ AdminRole.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pack;
 
-/**
- *
- * @author Hannah Emad
- */
 public class AdminRole {
-    
+    private EmployeeUserDatabase database;
+
+    public AdminRole() {
+        database = new EmployeeUserDatabase();
+    }
+
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        EmployeeUser newEmployee = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+        database.addEmployee(newEmployee);
+    }
+
+    public EmployeeUser[] getListOfEmployees() {
+        return database.getAllEmployees();
+    }
+
+    public void removeEmployee(String key) {
+        database.removeEmployeeById(key);
+    }
+
+    public void logout() {
+        database.saveToFile();
+    }
 }
