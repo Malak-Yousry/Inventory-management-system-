e99e6f205650a51ad7011370a1377ec5ed8c1408
Malak-Yousry <es.mlk.yousry2023@alexu.edu.eg>
2025-10-20 00:31:10 +0300

    Edits on AdminRole

---
+++ pack/AdminRole.java
@@ -0,0 +1,40 @@
+
+package pack;
+
+import java.util.ArrayList;
+public class AdminRole implements Logout {
+    // attributes
+    private EmployeeUserDatabase database;
+// constructor
+    public AdminRole() {
+        database = new EmployeeUserDatabase("Employees.txt");
+        database.readFromFile();
+    }
+// methods
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        EmployeeUser newEmployee = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+       if(!database.contains(employeeId)){
+        database.insertRecord(newEmployee);
+        database.saveToFile();
+       }
+        else
+        System.out.println("Employee already exists!");
+    }
+
+    public EmployeeUser[] getListOfEmployees() {
+        ArrayList<EmployeeUser> list=database.returnAllRecords();
+        return list.toArray(new EmployeeUser[list.size()]);
+    }
+    public void removeEmployee(String key) {
+        if(database.contains(key)){
+            database.deleteRecord(key);
+            database.saveToFile();
+        }
+        else 
+        System.out.println("Employee not found!");
+    }
+    public void logout() {
+        database.saveToFile();
+    }
+    
+}
